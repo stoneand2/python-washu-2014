f9068de9a51d3c08476659be9799698d5a9fc192
stoneand2 <stoneand2@users.noreply.github.com>
2014-08-11 16:22:16 -0500

    HW 2 Revisions

--- hw2/hw2_4.py
+++ hw2/hw2_4.py
@@ -4,7 +4,7 @@ class Portfolio(object):
 	
 	def __init__(self, cash_value = 0.0):
 		self.cash_value = cash_value
-		self.my_investments = {}
+		self.my_investments = {} # a dict to hold all current investments the user has
 		self.history_log = "User's portfolio iniitialized.\n"
 	
 	def addCash(self, value_to_add):
@@ -18,29 +18,29 @@ class Portfolio(object):
 		return self.cash_value
 	
 	def makeInvestment(self, number, what_investment, type = ""):
-		if self.cash_value < (number * what_investment.price):
+		if self.cash_value < (number * what_investment.price): # won't let user make an investment if they don't have enough money
 			print "Insufficient funds to complete transaction."
 			return None
 		
 		self.cash_value = self.cash_value - (number * what_investment.price)
 		self.history_log += ("\nAdded %r shares of %s to portfolio, now have $%r in portfolio." % (number, what_investment.name, self.cash_value))
 		
-		if type not in self.my_investments:
-			self.my_investments[type] = {}
+		if type not in self.my_investments:  # adds the investment type (e.g. stocks),  name, and how many to the user's investment dict
+			self.my_investments[type] = {} 
 			(self.my_investments[type])[what_investment.name] = number
-			return dict_of_investments
-		(self.my_investments[type])[what_investment.name] = number
+			return dict_of_investments  
+		(self.my_investments[type])[what_investment.name] = number # if investment type already in dict, just adds the name and how many
 		
 	def sellInvestment(self, what_investment, number, bottom = 0.9, top = 1.2, x = 1, type = ""): # default selling values are for the mut. funds
 		
-		selling_random = np.random.uniform(bottom * x, top * x, size=1)
- 		(self.my_investments[type])[what_investment] = float("%.2f" %(((self.my_investments[type])[what_investment]) - number))
+		selling_random = np.random.uniform(bottom * x, top * x, size=1)  # uniformly draws the price from range
+ 		(self.my_investments[type])[what_investment] = float("%.2f" %(((self.my_investments[type])[what_investment]) - number))  # updates the amount of a investment the user has
 			
  		self.cash_value = self.cash_value + (number * (selling_random))
 		self.cash_value = float("%.2f" % self.cash_value)
 		self.history_log += ("\nSold %d shares of %s, now have $%r in portfolio." % (number, what_investment, self.cash_value))
 	
-	def buyStock(self, number, what_stock, type = "stocks"):
+	def buyStock(self, number, what_stock, type = "stocks"): # moves up to the more abstract makeInvestment with its stock-specific type information
 		self.makeInvestment(number, what_stock, type)
 	
 	def buyMutualFund(self, number, what_mf, type = "mutual funds"):
@@ -50,7 +50,7 @@ class Portfolio(object):
 		self.makeInvestment(number, what_bond, type)
 	
 	def sellStock(self, what_investment, number):
-		self.sellInvestment(what_investment, number, bottom = 0.5, top = 1.5, x = dict_of_investments['stocks'][what_investment], type="stocks") # replaces def. values in sellInvestment with those appropriate for selling stocks
+		self.sellInvestment(what_investment, number, bottom = 0.5, top = 1.5, x = dict_of_investments['stocks'][what_investment], type="stocks") # replaces defined values in sellInvestment with those appropriate for selling stocks
 	
 	def sellMutualFund(self, what_investment, number):
 		self.sellInvestment(what_investment, number, type = "mutual funds")
@@ -61,7 +61,7 @@ class Portfolio(object):
 	def history(self):
 		print self.history_log
 	
-	def __str__(self):
+	def __str__(self):  # the print(portfolio) method
 		what_to_print = "cash: $%r \n" % (self.cash_value)
 		for type in self.my_investments:
 			what_to_print += "%s: " % (type)
@@ -80,11 +80,11 @@ class Investments(object): # a parent class for stocks, mutual funds, bonds
 		self.type = type
 	
 	def addToDict(self):
-		if self.type not in dict_of_investments:
+		if self.type not in dict_of_investments:  # adds an investment type (e.g. stocks), investment name and the cost to purchase to the dict
 			dict_of_investments[self.type] = {}
 			(dict_of_investments[self.type])[self.name] = self.price
 			return dict_of_investments
-		(dict_of_investments[self.type])[self.name] = self.price
+		(dict_of_investments[self.type])[self.name] = self.price # investment type already in the dict, so it just adds the investment name and price
 	
 class Stock(Investments):
 	def __init__(self, price, name, type = "stocks"):
@@ -96,7 +96,7 @@ class MutualFund(Investments):
 		Investments.__init__(self, price, name, type)
 		self.addToDict()
 		
-class Bond(Investments): # introducing bonds is this easy. This treats bonds w/ the same rules as mutual funds, but could be altered like for stocks
+class Bond(Investments): # introducing bonds isn't hard w/ inheritance, just treat it like stocks and mutual funds. This treats bonds w/ the same rules as mutual funds, but could be altered like for stocks
 	def __init__(self, name, price = 1, type = "bonds"):
 		Investments.__init__(self, price, name, type)
 		self.addToDict()
@@ -108,5 +108,4 @@ portfolio = Portfolio()
 # mf1 = MutualFund("BRT")
 # mf2 = MutualFund("GHT")
 # portfolio.buyMutualFund(10.3, mf1)
-# portfolio.buyMutualFund(2, mf2)
-# portfolio.buyMutualFund(
+
---
+++ hw2/hw2_test.py
@@ -0,0 +1,70 @@
+import unittest
+import hw2_4
+
+class PortfolioTest(unittest.TestCase):
+	
+	def setup(self): # this will run before each test
+		pass
+	
+	def test_addCash(self):
+		self.assertEqual(300.50, hw2_4.portfolio.addCash(300.50))
+	
+	def test_createStock(self):
+		s = hw2_4.Stock(20, "HFH")
+		self.assertTrue("stocks" in hw2_4.dict_of_investments)
+		self.assertTrue("HFH" in hw2_4.dict_of_investments["stocks"])
+	
+	def test_createMutualFund(self):
+		mf1 = hw2_4.MutualFund("BRT")
+		self.assertTrue("mutual funds" in hw2_4.dict_of_investments)
+		self.assertTrue("BRT" in hw2_4.dict_of_investments["mutual funds"])
+	
+	def test_createBond(self):
+		bond1 = hw2_4.Bond("XYZ")
+		self.assertTrue("bonds" in hw2_4.dict_of_investments)
+		self.assertTrue("XYZ" in hw2_4.dict_of_investments["bonds"])
+		
+	def test_buyStock(self):
+		hw2_4.portfolio.buyStock(5, hw2_4.Stock(20, "HFH"))
+		self.assertTrue("stocks" in hw2_4.portfolio.my_investments)
+		self.assertTrue("HFH" in hw2_4.portfolio.my_investments["stocks"])
+		self.assertEqual(5, hw2_4.portfolio.my_investments["stocks"]["HFH"])
+		
+	def test_buyMutualFund(self):
+		hw2_4.portfolio.buyMutualFund(10.3, hw2_4.MutualFund("BRT"))
+		self.assertTrue("mutual funds" in hw2_4.portfolio.my_investments)
+		self.assertTrue("BRT" in hw2_4.portfolio.my_investments["mutual funds"])
+		self.assertEqual(10.3, hw2_4.portfolio.my_investments["mutual funds"]["BRT"])
+	
+	def test_buyBond(self):
+		hw2_4.portfolio.buyBond(11, hw2_4.Bond("XYZ"))
+		self.assertTrue("bonds" in hw2_4.portfolio.my_investments)
+		self.assertTrue("XYZ" in hw2_4.portfolio.my_investments["bonds"])
+		self.assertEqual(11, hw2_4.portfolio.my_investments["bonds"]["XYZ"])
+
+	def test_sellStock(self):
+		hw2_4.portfolio.sellStock("HFH", 2)
+		self.assertEqual(3, hw2_4.portfolio.my_investments["stocks"]["HFH"])
+		
+	def test_sellMutualFund(self):
+		hw2_4.portfolio.sellMutualFund("BRT", 3)
+		self.assertEqual(7.3, hw2_4.portfolio.my_investments["mutual funds"]["BRT"])
+		
+	def test_sellBond(self):
+		hw2_4.portfolio.sellBond("XYZ", 7)
+		self.assertEqual(4, hw2_4.portfolio.my_investments["bonds"]["XYZ"])
+		
+	def test_printPortfolio(self):
+		self.assertTrue("cash: " in hw2_4.portfolio.__str__())
+		self.assertTrue("stocks: " in hw2_4.portfolio.__str__())
+		self.assertTrue("mutual funds: " in hw2_4.portfolio.__str__())
+		self.assertTrue("bonds: " in hw2_4.portfolio.__str__())
+		self.assertTrue("HFH" in hw2_4.portfolio.__str__())
+		self.assertTrue("BRT" in hw2_4.portfolio.__str__())
+		self.assertTrue("XYZ" in hw2_4.portfolio.__str__())
+			
+	def test_withdrawCash(self):
+ 		self.assertEqual((hw2_4.portfolio.cash_value - 50), hw2_4.portfolio.withdrawCash(50))
+	
+if __name__ == '__main__': # run the test for us
+	unittest.main()	
